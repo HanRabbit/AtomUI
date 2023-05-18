bb50ad300703b04ebd2247b329ff496f1ff37978
HanRabbit <tianzihan5357@163.com>
2023-05-18 22:47:42 +0800

    committed

---
+++ src/ui/pages/home/home_events.h
@@ -0,0 +1,16 @@
+#ifndef ATOM_UI_HOME_EVENTS_H
+#define ATOM_UI_HOME_EVENTS_H
+
+#include <lvgl.h>
+//#include <src/extra/others/msg/lv_msg.h>
+#include <Arduino.h>
+#include "ui/utils/ui_utils.h"
+#include "common/time/time.h"
+
+void battery_status_show(lv_timer_t *timer);
+
+void ui_event(lv_event_t *e);
+
+void ui_events_handle();
+
+#endif //ATOM_UI_HOME_EVENTS_H
